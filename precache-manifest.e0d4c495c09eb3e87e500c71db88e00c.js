f9e24b8f4ff8e2218445d1bbdadbba690bf3b511
Daniel Luke Tanoeihusada <danielluke08@gmail.com>
2020-07-25 19:17:34 -0700

    Updates

---
+++ precache-manifest.e0d4c495c09eb3e87e500c71db88e00c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86402018d229e7bdacb8821264abef95",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "bdee0a76c32270fe3668",
+    "url": "/react-portfolio/static/css/main.fdfd450e.chunk.css"
+  },
+  {
+    "revision": "7887844475de6214c389",
+    "url": "/react-portfolio/static/js/2.59e430b5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.59e430b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdee0a76c32270fe3668",
+    "url": "/react-portfolio/static/js/main.e71dd393.chunk.js"
+  },
+  {
+    "revision": "a05a49d9351ba3961537",
+    "url": "/react-portfolio/static/js/runtime-main.bc59954b.js"
+  },
+  {
+    "revision": "86b4b123ca22618c72fdfb9a86262d9b",
+    "url": "/react-portfolio/static/media/DanielTanoeihusada.86b4b123.pdf"
+  },
+  {
+    "revision": "772d030f44719f76d7927f11f637240e",
+    "url": "/react-portfolio/static/media/aboutMe.772d030f.jpg"
+  },
+  {
+    "revision": "81cf41ef63f50cac6bb409303b4662fe",
+    "url": "/react-portfolio/static/media/background1.81cf41ef.jpg"
+  },
+  {
+    "revision": "5fbce81e26f2c4f1d3d3cea5975745ff",
+    "url": "/react-portfolio/static/media/budget-tracker.5fbce81e.png"
+  },
+  {
+    "revision": "987b9f3290c2d49e64b13fadb90747d4",
+    "url": "/react-portfolio/static/media/employee-directory.987b9f32.png"
+  },
+  {
+    "revision": "7697a2c304872aa2814b5134e17d7089",
+    "url": "/react-portfolio/static/media/fitness-tracker.7697a2c3.png"
+  },
+  {
+    "revision": "c2f0290d3ab94f6567f111ccdd992aaf",
+    "url": "/react-portfolio/static/media/notetaker.c2f0290d.png"
+  },
+  {
+    "revision": "f0e82440b22a735d3874a7f30cd08277",
+    "url": "/react-portfolio/static/media/spending.f0e82440.png"
+  },
+  {
+    "revision": "aca1cc6261311b3123ab53884cdcffd8",
+    "url": "/react-portfolio/static/media/treehouse.aca1cc62.png"
+  }
+]);
